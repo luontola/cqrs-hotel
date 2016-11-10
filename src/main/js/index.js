08dbdf7c881bec6649a53fe48e10ad3ad5277ca0
Esko Luontola <esko@luontola.fi>
2016-11-10 21:53:02 +0200

    Use PureCSS

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,6 +2,7 @@
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
+import "purecss/build/pure-min.css";
 import "../css/style.css";
 import "whatwg-fetch";
 import React from "react";
