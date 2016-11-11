401914ba35b01a8238e87407f353dda3ff604338
Esko Luontola <esko@luontola.fi>
2016-11-11 17:24:17 +0200

    Log saved events

--- src/main/java/fi/luontola/cqrshotel/framework/InMemoryEventStore.java
+++ src/main/java/fi/luontola/cqrshotel/framework/InMemoryEventStore.java
@@ -4,6 +4,9 @@
 
 package fi.luontola.cqrshotel.framework;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -11,6 +14,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class InMemoryEventStore implements EventStore {
+
+    private static final Logger log = LoggerFactory.getLogger(InMemoryEventStore.class);
 
     private final ConcurrentMap<UUID, List<Event>> streamsById = new ConcurrentHashMap<>();
 
@@ -31,7 +36,11 @@ public class InMemoryEventStore implements EventStore {
             if (expectedVersion != actualVersion) {
                 throw new OptimisticLockingException("expected version " + expectedVersion + " but was " + actualVersion + " for stream " + streamId);
             }
-            events.addAll(newEvents);
+            for (Event newEvent : newEvents) {
+                events.add(newEvent);
+                int newVersion = events.size();
+                log.info("Saved stream {} version {}: {}", streamId, newVersion, newEvent);
+            }
         }
     }
 }
