0e118b91710c10814718317c22c25f6a088a5eae
Esko Luontola <esko@luontola.fi>
2016-11-12 21:50:07 +0200

    Namespaced actions

--- src/main/js/reservationActions.js
+++ src/main/js/reservationActions.js
@@ -2,13 +2,15 @@
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
-export const RESERVATION_OFFER_RECEIVED = 'RESERVATION_OFFER_RECEIVED';
+const namespace = 'cqrs-hotel/';
+
+export const RESERVATION_OFFER_RECEIVED = namespace + 'RESERVATION_OFFER_RECEIVED';
 export const reservationOfferReceived = (offer) => ({
   type: RESERVATION_OFFER_RECEIVED,
   offer
 });
 
-export const RESERVATION_MADE = 'RESERVATION_MADE';
+export const RESERVATION_MADE = namespace + 'RESERVATION_MADE';
 export const reservationMade = (reservation) => ({
   type: RESERVATION_MADE,
   reservation
