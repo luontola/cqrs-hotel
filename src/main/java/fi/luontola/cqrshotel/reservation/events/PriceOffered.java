3bc4eb6bc92c32297cb4ff56d459b856a91c8318
Esko Luontola <esko@luontola.fi>
2017-01-17 15:34:33 +0200

    Use year range in copyright statements

--- src/main/java/fi/luontola/cqrshotel/reservation/events/PriceOffered.java
+++ src/main/java/fi/luontola/cqrshotel/reservation/events/PriceOffered.java
@@ -1,4 +1,4 @@
-// Copyright © 2017 Esko Luontola
+// Copyright © 2016-2017 Esko Luontola
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
