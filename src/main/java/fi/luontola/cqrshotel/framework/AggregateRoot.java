b4c60e718139508ea57f789eb7610086d64a5614
Esko Luontola <esko@luontola.fi>
2017-10-13 15:52:49 +0300

    Improve documentation

--- src/main/java/fi/luontola/cqrshotel/framework/AggregateRoot.java
+++ src/main/java/fi/luontola/cqrshotel/framework/AggregateRoot.java
@@ -11,6 +11,23 @@ import java.util.UUID;
 
 import static fi.luontola.cqrshotel.framework.EventListeners.Requirements.MUST_BE_PRIVATE;
 
+/**
+ * Aggregate root is the consistency boundary for doing atomic transactions.
+ * You may obtain {@code AggregateRoot} instances from a {@link Repository}.
+ * <p>
+ * The methods of an {@code AggregateRoot} implementation MUST follow a strong dichotomy of
+ * event listeners and command handlers:
+ * <p>
+ * <b>Event listeners</b> are private methods, annotated with {@link EventListener},
+ * which take an {@link Event} as parameter. Their purpose is to read data from previously published events.
+ * The framework will call them automatically for all old and newly published events of this aggregate.
+ * <em>Event listeners MAY mutate state and MUST NOT throw exceptions.</em>
+ * <p>
+ * <b>Command handlers</b> are public methods which take arbitrary parameters and are called by users.
+ * Their purpose is to validate business rules and publish new events using {@link #publish(Event)}.
+ * The published events will be persisted atomically by {@link Repository#save}.
+ * <em>Command handlers MUST NOT mutate state and MAY throw exceptions.</em>
+ */
 public abstract class AggregateRoot {
 
     private final EventListeners eventListeners;
