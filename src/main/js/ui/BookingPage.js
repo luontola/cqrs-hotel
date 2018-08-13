3556587b70447e5be307e76d8338c0e5ecb528f3
Esko Luontola <esko@luontola.fi>
2018-08-13 15:33:18 +0300

    Refactor

--- src/main/js/ui/BookingPage.js
+++ src/main/js/ui/BookingPage.js
@@ -1,4 +1,4 @@
-// Copyright © 2016-2017 Esko Luontola
+// Copyright © 2016-2018 Esko Luontola
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
@@ -18,7 +18,10 @@ const BookingPage = ({reservation, reservationOffer, makeReservation}) => (
       <b>Departure:</b> {reservationOffer.departure}<br/>
       {reservationOffer.totalPrice ? <div>
         <b>Cost:</b> {reservationOffer.totalPrice}<br/>
-        <button type="button" onClick={() => makeReservation(reservationOffer)}>Make Reservation</button>
+        <button type="button" // TODO: fields for inputting name and email
+                onClick={() => makeReservation({...reservationOffer, name: "John Doe", email: "john@example.com"})}>
+          Make Reservation
+        </button>
       </div> : <div style={{color: 'red'}}>
         Sold out!
       </div>}
@@ -40,14 +43,10 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    makeReservation: (reservationOffer) => {
-      api.post('/api/make-reservation', {
-        ...reservationOffer,
-        name: "John Doe",
-        email: "john@example.com",
-      })
+    makeReservation: (params) => {
+      api.post('/api/make-reservation', params)
         .then(response => {
-          const {reservationId} = reservationOffer;
+          const {reservationId} = params;
           history.push({pathname: `/reservations/${reservationId}`});
           // TODO: empty the state; must not be able to go back and make the same reservation again
         })
